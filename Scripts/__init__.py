5a1f44e033555d8727e1c476f7e9bb58d9316078
Zachary Labe <zlabe@uci.edu>
2018-01-16 14:27:15 -0800

    2011-2017

--- Scripts/__init__.py
+++ Scripts/__init__.py
@@ -2,7 +2,7 @@
 The Python repository "CAAthickness" contains the scripts, data, 
 figures, and text for an intercomparison study of Arctic sea ice 
 thickness in the Canadian Arctic Archipelago. Attention is given
-to first-year ice. We focus on the period from April 2010-2016
+to first-year ice. We focus on the period from April 2010-2017
 for sea ice thickness and snow depth. A variety of data sets
 are assessed (including CryoSat-2, PIOMAS, Warren snow climo)
 Author: Zachary Labe (zlabe@uci.edu)
